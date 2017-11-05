8579b1a7f71b1813748dfecc717a02f442970ba0
Michael Alaev <alaev.michael@gmail.com>
2017-11-05 23:30:15 +0200

    removed user password and email from get user by id

--- server/controllers/users/getUser.js
+++ server/controllers/users/getUser.js
@@ -4,7 +4,7 @@ const Users = require('../../models/users');
 module.exports = async (req, res) => {
   try {
     const userId = req.params.userId;
-    const user = await Users.findById({ _id: userId }, { __v: 0, password: 0, _id: 0 });
+    const user = await Users.findById({ _id: userId }, { __v: 0, local:0, method:0, _id: 0 });
     res.status(200).json({ success: true, user });
   } catch(error) {
     res.status(400).json({ success: false, message: MESSAGES.USER_NOT_FOUND });
