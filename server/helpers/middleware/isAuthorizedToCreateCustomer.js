2448e512b7705fc1082b012986e87a6b790eceba
Andrey Kishtov <andreykishtov@gmail.com>
2017-11-08 19:40:27 +0200

    added roles and create autorization middleware

---
+++ server/helpers/middleware/isAuthorizedToCreateCustomer.js
@@ -0,0 +1,23 @@
+const JWT = require('jsonwebtoken');
+const MESSAGES = require('../helpers/messages');
+const ROLES = require('../roles');
+exports.isAuthorizedUser = (req, res, next) => {
+  const { authorization } = req.headers;
+  const { token } = req.body;
+
+  const userRole = authorization || token;
+
+  if (!userRole) {
+    return res.status(401).json({ message: MESSAGE.NOT_AUTHORIZED });
+  }
+
+  const decodedToken = JWT.decode(userRole, { complete: true });
+
+  const { role } = decodedToken.payload;
+
+  if (role != ROLES.admin || role != ROLES.developer || role != ROLES.moderator) {
+    return  res.status(401).json({ message: MESSAGE.NOT_AUTHORIZED });
+  }
+
+  return next();
+};
