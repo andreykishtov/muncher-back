365c489bcba94255b18fab498fbbcd44dea37959
Michael Alaev <alaev.michael@gmail.com>
2017-11-10 13:44:29 +0200

    moved functions that are not async outside try block

--- server/controllers/users/register.js
+++ server/controllers/users/register.js
@@ -4,19 +4,23 @@ const signToken = require('./signToken');
 
 module.exports = async (req, res) => {
   let userRole = null;
-  try {
-    const { email, password } = req.value.body;
-    if(!email) {
-      return res.status(200).json({ message: MESSAGES.EMAIL_REQUIRED });
-    }
-    if(!password) {
-      return res.status(200).json({ message: MESSAGES.PASSWORD_REQUIRED });
-    }
+  const { email, password } = req.value.body;
 
-    if(!req.body.role || !req.value.body.role) {
-      userRole = 1
-    }
+  if(!email) {
+    return res.status(200).json({ message: MESSAGES.EMAIL_REQUIRED });
+  }
+
+  if(!password) {
+    return res.status(200).json({ message: MESSAGES.PASSWORD_REQUIRED });
+  }
+
+  if(!req.body.role || !req.value.body.role) {
+    userRole = 1
+  }
+
+  try {
     const isExisting = await Users.findOne({ 'local.email': email });
+
     if(isExisting) {
       return res.status(200).json({ error: MESSAGES.EMAIL_TAKEN });
     }
