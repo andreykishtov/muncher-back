3146076943a265d386e1b92602a26a1345685e41
Michael Alaev <alaev.michael@gmail.com>
2017-12-23 00:00:01 +0200

    added test setup

--- server/routes/api/v1/location.test.js
+++ server/routes/api/v1/location.test.js
@@ -1,10 +1,60 @@
-const server = require('../../../index');
-const test = require('ava');
-const http = require('ava-http');
-const locationApi = 'http://localhost:3000/api/locations';
+const server = require('../../../app');
+const http = require('http');
+const Location = require('../../../models/location');
+const axios = require('axios');
+const faker = require('faker');
+const _ = require('lodash');
 
-test('it should get locations', async t => {
-    const res = await http.get(`${locationApi}`);
-    t.true(typeof res === 'object');
-    t.is(typeof res.body, 'array');
+let app;
+let port;
+let locationApi;
+
+describe('location route', () => {
+  beforeEach(() => {
+    port = faker.random.number({ min: 4000, max: 6553 });
+    locationApi = `http://localhost:${port}/api/v1/location/`;
+    app = http.createServer(server).listen(port);
+  });
+
+  afterEach(async () => {
+    await Location.remove({});
+  });
+
+  test('should return empty array when no location present', async () => {
+    const { data } = await axios.get(`http://localhost:${port}/api/v1/location/`);
+    expect(data.message).toBe('success');
+    expect(data.locations.length).toBe(0);
+  });
+
+  test('should return five location', async () => {
+    let location;
+    _.times(5, async () => {
+      location = {
+        name: faker.lorem.word(),
+        type: faker.lorem.word(),
+        generalDesc: faker.lorem.word(),
+        imageUrl: faker.lorem.word(),
+        address: {
+          country: faker.address.county(),
+          city: faker.address.city(),
+          street: faker.address.streetName(),
+          number: faker.address.zipCode(),
+          lat: faker.address.latitude(),
+          lng: faker.address.longitude()
+        }
+      };
+      await new Location(location).save();
+    });
+    const { data } = await axios.get(`http://localhost:${port}/api/v1/location/`);
+    expect(data.message).toBe('success');
+    expect(data.locations.length).toBe(5);
+  });
+
+  test('should return one location with correct data', async () => {
+    const NEW_LOCATION = await new Location({ name: faker.address.streetName() }).save();
+    const { data } = await axios.get(`http://localhost:${port}/api/v1/location/`);
+    const { locations, updateDate, createDate } = data;
+    expect(data.message).toBe('success');
+    expect(_.first(locations).name).toBe(NEW_LOCATION.name);
+  });
 });
